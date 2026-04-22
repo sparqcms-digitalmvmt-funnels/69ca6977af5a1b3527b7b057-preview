b1f7781300d7bb0e286ac30e0b897b5cbce698fa
Don E <DRElliston@users.noreply.github.com>
2026-04-22 06:59:36 -0500

    deploy funnel preview

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -609,7 +609,7 @@ async function createOrderViaWallet(confirmationToken, paymentMethodId) {
         ?.getAttribute("data-shipping-profile-id") || undefined;
 
   const orderData = {
-    pageId: "m9sWmaxCYYmtTSfukQgS8duN-PQopdKDMq0DBE4IT5tdx5eZUGeHN_00WvmnBfgi",
+    pageId: "svVzBCkmCj3MolZNxh7uRkY3DOZAHICOAMpSgxcoA-egAkbbO-R1HIGwTqXlfGkT",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1,
@@ -1763,7 +1763,7 @@ async function createOrderViaPaypal(isExpress = false) {
   const shippingProfileId = +document.querySelector(`[data-product-id="${selectedProduct.id}"]`)?.getAttribute('data-shipping-profile-id') || undefined;
   const sameAddress = isSameAddress();
   const orderData = {
-    pageId: "m9sWmaxCYYmtTSfukQgS8duN-PQopdKDMq0DBE4IT5tdx5eZUGeHN_00WvmnBfgi",
+    pageId: "svVzBCkmCj3MolZNxh7uRkY3DOZAHICOAMpSgxcoA-egAkbbO-R1HIGwTqXlfGkT",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -2062,7 +2062,7 @@ async function createOrderViaKlarna() {
   const sameAddress = isSameAddress();
 
   const orderData = {
-    pageId: "m9sWmaxCYYmtTSfukQgS8duN-PQopdKDMq0DBE4IT5tdx5eZUGeHN_00WvmnBfgi",
+    pageId: "svVzBCkmCj3MolZNxh7uRkY3DOZAHICOAMpSgxcoA-egAkbbO-R1HIGwTqXlfGkT",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1,
     email: email,
@@ -2440,7 +2440,7 @@ async function createOrderViaCreditCard() {
   let orderTotal = Math.max(0, Number(selectedProduct.price) * selectedProduct.quantity);
 
   const orderData = {
-    pageId: "m9sWmaxCYYmtTSfukQgS8duN-PQopdKDMq0DBE4IT5tdx5eZUGeHN_00WvmnBfgi",
+    pageId: "svVzBCkmCj3MolZNxh7uRkY3DOZAHICOAMpSgxcoA-egAkbbO-R1HIGwTqXlfGkT",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -4532,7 +4532,7 @@ async function returnPaypal() {
 ;
 
     const body = {
-        pageId: "m9sWmaxCYYmtTSfukQgS8duN-PQopdKDMq0DBE4IT5tdx5eZUGeHN_00WvmnBfgi",
+        pageId: "svVzBCkmCj3MolZNxh7uRkY3DOZAHICOAMpSgxcoA-egAkbbO-R1HIGwTqXlfGkT",
         action: "process",
         campaign_id: CAMPAIGN_ID,
         connection_id: 1,
--- scripts/checkout2.js
+++ scripts/checkout2.js
@@ -381,7 +381,7 @@ async function returnPaypal() {
     const responseDataCustomer = await responseCustomer.json();
     const orderData = JSON.parse(sessionStorage.getItem("orderData"));
     const body = {
-      pageId: "4cHFmeNf8ZD8VLFdx4PoS6cL-ohQkdzjZKets5eCQJmxWl7q4B21N1gu3io_p8iA",
+      pageId: "tVtmEWrGoe8_Zhpcv7zBNpvEqUF4JqpcRuZSuH5EdoIVitaJdZA0QbHX2y35ElgX",
       action: "process",
       campaign_id: CAMPAIGN_ID,
       connection_id: 1,
@@ -1099,7 +1099,7 @@ async function createOrderViaPaypal() {
   const [exp_month, exp_year] = expirationDate.split("/");
   const billShipSameCheckbox = document.getElementById("billShipSame");
   const orderData = {
-    pageId: "4cHFmeNf8ZD8VLFdx4PoS6cL-ohQkdzjZKets5eCQJmxWl7q4B21N1gu3io_p8iA",
+    pageId: "tVtmEWrGoe8_Zhpcv7zBNpvEqUF4JqpcRuZSuH5EdoIVitaJdZA0QbHX2y35ElgX",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -1248,7 +1248,7 @@ async function createOrderViaCreditCard() {
   const [exp_month, exp_year] = expirationDate.split("/");
   const billShipSameCheckbox = document.getElementById("billShipSame");
   const orderData = {
-    pageId: "4cHFmeNf8ZD8VLFdx4PoS6cL-ohQkdzjZKets5eCQJmxWl7q4B21N1gu3io_p8iA",
+    pageId: "tVtmEWrGoe8_Zhpcv7zBNpvEqUF4JqpcRuZSuH5EdoIVitaJdZA0QbHX2y35ElgX",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -1428,7 +1428,7 @@ async function createOrderViaCreditCard() {
   async function sendLead() {
   const formData = new FormData(formEl);
   const orderData = {
-    pageId: "4cHFmeNf8ZD8VLFdx4PoS6cL-ohQkdzjZKets5eCQJmxWl7q4B21N1gu3io_p8iA",
+    pageId: "tVtmEWrGoe8_Zhpcv7zBNpvEqUF4JqpcRuZSuH5EdoIVitaJdZA0QbHX2y35ElgX",
     connection_id: 1,
     campaignId: CAMPAIGN_ID,
     first_name: formData.get("firstName"),
