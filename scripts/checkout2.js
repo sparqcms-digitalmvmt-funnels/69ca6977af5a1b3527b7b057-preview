7d60d0dc95d0efa0cf853a87ce3ca92cc8dc9cee
Don E <DRElliston@users.noreply.github.com>
2026-04-22 07:24:51 -0500

    deploy funnel preview

--- scripts/checkout2.js
+++ scripts/checkout2.js
@@ -381,7 +381,7 @@ async function returnPaypal() {
     const responseDataCustomer = await responseCustomer.json();
     const orderData = JSON.parse(sessionStorage.getItem("orderData"));
     const body = {
-      pageId: "kx6Atg37ThLU7vvUJmQjHAHoBiofbJen4d69WG8XC8aVNC-0UT9aXkGcdWl-N6tm",
+      pageId: "J-N9hoMPwviBkjvB7tCZnlwGbJtAd6defgkoDoA3r847FRMODfBB2xC8ozQZzC0I",
       action: "process",
       campaign_id: CAMPAIGN_ID,
       connection_id: 1,
@@ -1099,7 +1099,7 @@ async function createOrderViaPaypal() {
   const [exp_month, exp_year] = expirationDate.split("/");
   const billShipSameCheckbox = document.getElementById("billShipSame");
   const orderData = {
-    pageId: "kx6Atg37ThLU7vvUJmQjHAHoBiofbJen4d69WG8XC8aVNC-0UT9aXkGcdWl-N6tm",
+    pageId: "J-N9hoMPwviBkjvB7tCZnlwGbJtAd6defgkoDoA3r847FRMODfBB2xC8ozQZzC0I",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -1248,7 +1248,7 @@ async function createOrderViaCreditCard() {
   const [exp_month, exp_year] = expirationDate.split("/");
   const billShipSameCheckbox = document.getElementById("billShipSame");
   const orderData = {
-    pageId: "kx6Atg37ThLU7vvUJmQjHAHoBiofbJen4d69WG8XC8aVNC-0UT9aXkGcdWl-N6tm",
+    pageId: "J-N9hoMPwviBkjvB7tCZnlwGbJtAd6defgkoDoA3r847FRMODfBB2xC8ozQZzC0I",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -1428,7 +1428,7 @@ async function createOrderViaCreditCard() {
   async function sendLead() {
   const formData = new FormData(formEl);
   const orderData = {
-    pageId: "kx6Atg37ThLU7vvUJmQjHAHoBiofbJen4d69WG8XC8aVNC-0UT9aXkGcdWl-N6tm",
+    pageId: "J-N9hoMPwviBkjvB7tCZnlwGbJtAd6defgkoDoA3r847FRMODfBB2xC8ozQZzC0I",
     connection_id: 1,
     campaignId: CAMPAIGN_ID,
     first_name: formData.get("firstName"),
